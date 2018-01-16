4dca8e2162ba9bec2e44a7fb3ffa95b925f5b2a9
saracaparros <s.caparros@sapalomera.cat>
2018-01-16 18:40:17 +0100

    sentencies sql

---
+++ M02-UF3/Activitat 1/sentencies part1.sql
@@ -0,0 +1,66 @@
+#1. Obtenir el nom i l’adreça dels hotels de 4 estrelles.
+
+
+#2. Obtenir el nom dels clients (Nom i cognom) que el seu cognom comenci per vocal (sense tenir en compte els accents).
+
+
+#3. Quina és la reserva_id que té més nits. Indica també la quantitat de nits.
+
+
+#4. Quantes reserves va rebre l’hotel ‘Catalonia Ramblas’ de Barcelona durant tot  l’any 2015 (una reserva pertany al 2015 si alguna nit d’aquesta reserva era del 2015).
+
+
+#5. Obtenir el nom i cognoms dels clients que varen néixer el mes de Març.
+
+
+#6. Quantitat d’hotels de 4 estrelles de la població de Barcelona.
+
+
+#7. De l’any 2015 volem obtenir els seu histograma de reserves. És a dir volem saber el número de reserves de cadascun dels mesos. Una reserva pertany a un mes si la alguna nit d’aquella reserva cau a dins de l’any 2015.
+
+
+#8. El nom dels hotels que tenen com a mínim una habitació lliure durant les dates ‘2015-05-01’ i ‘2015-05-17’.
+
+
+#9. Obtenir la quantitat de reserves que s’inicien en cadascun dels dies de la setmana. Tenint en compte només l’any 2016.
+
+
+#10. Durant 2014 qui va realitzar més reserves? Els homes o les dones? Mostra el sexe i el número de reserves.
+
+
+#11. Quina és la mitjana de dies de reserva per l’hotel «HTOP Royal Star» de Blanes durant l’any 2016? (Una reserva pertany el 2016 si alguna nit cau en aquest any).
+
+
+#12. El nom, categoria, adreça i número d’habitacions de l’hotel amb més habitacions de la BD.
+
+
+#13. Rànquing de 5 països amb més reserves durant l’any 2016. Per cada país mostrar el nom del país i el número de reserves.
+
+
+#14. Codi client, Nom, Cognom, del client que ha realitzat més reserves de tota la BD.
+
+
+#15. Codi client, Nom, Cognom, del client que ha realitzat més reserves durant el mes d’agost de l’any 2016. Les reserves a comptabilitzar són totes aquelles que en algun dia del seu període cau en el mes d’agost.
+
+
+#16. Quin és el país que en tenim menys clients?
+
+
+#17. Quina és la mitjana de nits dels clients provinents d’‘HOLANDA’ per l’any 2016?
+
+
+#18. Digues el nom i cognoms dels clients que el seu cognom sigui ‘Bahi’.
+
+
+#19. Quins clients (nom, cognoms) segueixen el patró de que el seu cognom comenci per la lletra ‘p’  i seguida d’una vocal.
+
+
+#20. Quin és l’hotel de 4 estrelles amb més reserves durant tot el 2015 ( una reserva pertany el 2015 si alguna de les nits hi pertany).
+
+
+#21. Quin és l’hotel amb més reserves (tota la BD).
+
+
+#22. Quin és el país amb més reserves? (tots els anys) O sigui, quin és el país d’on han vingut més turistes.
+
+
